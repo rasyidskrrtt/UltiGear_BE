116205b75e77e78c4b90078473e414d0328293d7
Your Name <you@example.com>
2024-12-17 09:34:31 +0700

    edit code

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,15 +1,15 @@
-const env = require('./env');
-const cloudinary = require('cloudinary').v2
+// const env = require('./env');
+// const cloudinary = require('cloudinary').v2
 
-const initCloudinary = () => {
+// const initCloudinary = () => {
 
-    cloudinary.config({ 
-        cloud_name: env.cldCloudName, 
-        api_key: env.cldApiKey, 
-        api_secret: env.cldApiSecret
-    });
+//     cloudinary.config({ 
+//         cloud_name: env.cldCloudName, 
+//         api_key: env.cldApiKey, 
+//         api_secret: env.cldApiSecret
+//     });
 
-    return cloudinary
-}
+//     return cloudinary
+// }
 
-module.exports = initCloudinary
+// module.exports = initCloudinary
--- src/config/env.js
+++ src/config/env.js
@@ -5,9 +5,9 @@ module.exports = {
   mongodbUri: process.env.MONGODB_URI,
   jwtSecret: process.env.JWT_SECRET,
   jwtExpiresIn: process.env.JWT_EXPIRES_IN,
-  cldCloudName: process.env.CLOUDINARY_CLOUD_NAME,
-  cldApiKey: process.env.CLOUDINARY_API_KEY,
-  cldApiSecret: process.env.CLOUDINARY_API_SECRET,
+  // cldCloudName: process.env.CLOUDINARY_CLOUD_NAME,
+  // cldApiKey: process.env.CLOUDINARY_API_KEY,
+  // cldApiSecret: process.env.CLOUDINARY_API_SECRET,
   midtransServerKey: process.env.MIDTRANS_SERVER,
   midtransClientkey: process.env.MIDTRANS_CLIENT
 };
